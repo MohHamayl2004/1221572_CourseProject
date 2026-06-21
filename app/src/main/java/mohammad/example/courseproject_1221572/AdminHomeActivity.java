06510514a9e42d46165c5b5f8a4cd51c461cf979
Mohammad Ajaj <mohammadhazem985@gmail.com>
2026-06-21 13:14:22 +0300

    add navigation drawer to admin panel

--- app/src/main/java/mohammad/example/courseproject_1221572/AdminHomeActivity.java
+++ app/src/main/java/mohammad/example/courseproject_1221572/AdminHomeActivity.java
@@ -2,69 +2,63 @@ package mohammad.example.courseproject_1221572;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.core.view.GravityCompat;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class AdminHomeActivity extends AppCompatActivity {
 
-    Button buttonManageUsers;
-    Button buttonAddAdmin;
-    Button buttonManageEvents;
-    Button buttonViewReservations;
-    Button buttonAdminLogout;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
-        buttonManageUsers = (Button) findViewById(R.id.buttonManageUsers);
-        buttonAddAdmin = (Button) findViewById(R.id.buttonAddAdmin);
-        buttonManageEvents = (Button) findViewById(R.id.buttonManageEvents);
-        buttonViewReservations = (Button) findViewById(R.id.buttonViewReservations);
-        buttonAdminLogout = (Button) findViewById(R.id.buttonAdminLogout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        buttonManageUsers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminHomeActivity.this, AdminUsersActivity.class);
-                startActivity(intent);
-            }
-        });
+        setSupportActionBar(toolbar);
 
-        buttonAddAdmin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminHomeActivity.this, AddAdminActivity.class);
-                startActivity(intent);
-            }
-        });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.app_name, R.string.app_name
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        buttonManageEvents.setOnClickListener(new View.OnClickListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminHomeActivity.this, AdminEventsActivity.class);
-                startActivity(intent);
-            }
-        });
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        buttonViewReservations.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminHomeActivity.this, AdminReservationsActivity.class);
-                startActivity(intent);
-            }
-        });
+                int id = item.getItemId();
 
-        buttonAdminLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AdminHomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                if (id == R.id.admin_users) {
+                    startActivity(new Intent(AdminHomeActivity.this, AdminUsersActivity.class));
+                } else if (id == R.id.admin_add) {
+                    startActivity(new Intent(AdminHomeActivity.this, AddAdminActivity.class));
+                } else if (id == R.id.admin_events) {
+                    startActivity(new Intent(AdminHomeActivity.this, AdminEventsActivity.class));
+                } else if (id == R.id.admin_reservations) {
+                    startActivity(new Intent(AdminHomeActivity.this, AdminReservationsActivity.class));
+                } else if (id == R.id.admin_logout) {
+                    Intent intent = new Intent(AdminHomeActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
     }
